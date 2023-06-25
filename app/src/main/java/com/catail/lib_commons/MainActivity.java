9cccbcee931c5352566aa7521d90b9d976591c6d
杜军委 <3032801873@qq.com>
2023-06-25 16:57:18 +0800

    first commit

---
+++ app/src/main/java/com/catail/lib_commons/MainActivity.java
@@ -0,0 +1,18 @@
+package com.catail.lib_commons;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.catail.lib_commons.utils.Logger;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Logger.e("测试");
+    }
+}
